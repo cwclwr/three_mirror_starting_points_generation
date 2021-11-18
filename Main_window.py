ee6d6a3a532c038bc5873ec413fbbb42e6b4efc1
cwclwr <cwclwr@163.com>
2021-11-18 19:34:26 +0800

    add try001 and try002 and two file

--- Main_window.py
+++ Main_window.py
@@ -7,28 +7,26 @@ import sys
 import pyqtgraph
 
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 class Main_window(QMainWindow):
     def __init__(self):
         super(Main_window,self).__init__()
+        self.iniUI()
+    def iniUI(self):
         # 设置主窗口的标题
         self.setWindowTitle('Three mirror starting points')
-        # self.setWindowTitle('')
+        # 设置窗口图标
+        self.setWindowIcon(QIcon('./images/Basilisk.ico'))
 
         # 设置窗口的尺寸
         self.resize(800,800)
         # 移动窗口位置
 
         self.move(300,300)
+        self.center_window()
 
-    # def center(self):
-    #     # 获取屏幕坐标系
-    #     screen = QDesktopWidget().screenGeometry()
-    #     # 获取窗口坐标系
-    #     size = self.geometry()
-    #     newLeft = (screen.width() - size.width()) / 2
-    #     newTop = (screen.height() - size.height()) / 2
-    #     self.move(newLeft, newTop)
+
 
     def center_window(self):
         screen_size = QDesktopWidget().screenGeometry()
@@ -46,6 +44,7 @@ class Main_window(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('./images/Dragon.ico'))
 
     window = Main_window()
     window.center_window()
